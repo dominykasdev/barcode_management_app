9637c59a616f4d8ccdb245d063281deb242523c6
dominykasdev <contact@dominykas.dev>
2024-10-07 16:43:23 +0100

    empty strings tests

--- src/components/BarcodeForm/BarcodeForm.validation/BarcodeForm.validation.test.tsx
+++ src/components/BarcodeForm/BarcodeForm.validation/BarcodeForm.validation.test.tsx
@@ -15,6 +15,7 @@ test("should validate format of string", () => {
   const numbersOnly: string = "1234567891011";
   const lettersOnly: string = "abcdefghijklm";
   const specialCharacters2: string = "Æ♣622373592UÉ";
+  const emptyString: string = "";
 
   expect(validateFormat(workingBarcode)).toBeTruthy();
   expect(validateFormat(tooShort)).toBeFalsy();
@@ -23,6 +24,7 @@ test("should validate format of string", () => {
   expect(validateFormat(specialCharacters2)).toBeFalsy();
   expect(validateFormat(numbersOnly)).toBeFalsy();
   expect(validateFormat(lettersOnly)).toBeFalsy();
+  expect(validateFormat(emptyString)).toBeFalsy();
 });
 
 test("should validate string for ISO country code", () => {
@@ -33,12 +35,14 @@ test("should validate string for ISO country code", () => {
   const tooShort: string = "EP6223592U";
   const tooLong: string = "EP622351412492US";
   const specialCharacter: string = "EP622373592?!";
+  const emptyString: string = "";
 
   expect(validateISOCC(existingISOCC)).toBeTruthy();
   expect(validateISOCC(nonExistentISOCC)).toBeFalsy();
   expect(validateISOCC(tooShort)).toBeFalsy();
   expect(validateISOCC(tooLong)).toBeFalsy();
   expect(validateISOCC(specialCharacter)).toBeFalsy();
+  expect(validateISOCC(emptyString)).toBeFalsy();
 });
 
 test("should validate string check digit", () => {
@@ -52,6 +56,7 @@ test("should validate string check digit", () => {
   const tooLong: string = "EP622351412492US";
   const specialCharacter: string = "EP622373592?!";
   const specialCharacter2: string = "EP62237359£US";
+  const emptyString: string = "";
 
   expect(validateCheckDigit(workingDigit)).toBeTruthy();
   expect(validateCheckDigit(workingDigitNumber0)).toBeTruthy();
@@ -62,6 +67,7 @@ test("should validate string check digit", () => {
   expect(validateCheckDigit(tooLong)).toBeFalsy();
   expect(validateCheckDigit(specialCharacter)).toBeTruthy(); // special character applied to letters not affected
   expect(validateCheckDigit(specialCharacter2)).toBeFalsy();
+  expect(validateCheckDigit(emptyString)).toBeFalsy();
 });
 
 test("should validate string for duplicates", () => {
