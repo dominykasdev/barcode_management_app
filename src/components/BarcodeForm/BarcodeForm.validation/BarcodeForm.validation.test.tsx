c0a1377969c9ebb4ad55d0dbe57ffd8bb096059e
dominykasdev <contact@dominykas.dev>
2024-10-07 15:41:25 +0100

    Barcode validation tests

---
+++ src/components/BarcodeForm/BarcodeForm.validation/BarcodeForm.validation.test.tsx
@@ -0,0 +1,78 @@
+import {
+  checkForDuplicates,
+  validateCheckDigit,
+  validateFormat,
+  validateISOCC,
+} from ".";
+import barcodesJSON from "../../../data/Barcodes.json";
+
+test("should validate format of string", () => {
+  // format should be 2 letters followed by 9 digits followed by 2 letters. Total of 13 characters
+  const workingBarcode: string = "EP622373592US";
+  const tooShort: string = "EP6223592U";
+  const tooLong: string = "EP622351412492US";
+  const specialCharacter: string = "¬P622373592U!";
+  const numbersOnly: string = "1234567891011";
+  const lettersOnly: string = "abcdefghijklm";
+  const specialCharacters2: string = "Æ♣622373592UÉ";
+
+  expect(validateFormat(workingBarcode)).toBeTruthy();
+  expect(validateFormat(tooShort)).toBeFalsy();
+  expect(validateFormat(tooLong)).toBeFalsy();
+  expect(validateFormat(specialCharacter)).toBeFalsy();
+  expect(validateFormat(specialCharacters2)).toBeFalsy();
+  expect(validateFormat(numbersOnly)).toBeFalsy();
+  expect(validateFormat(lettersOnly)).toBeFalsy();
+});
+
+test("should validate string for ISO country code", () => {
+  // Last 2 letters of 13 character string should be ISO country code.
+  // These are compared to match from list on ISOCountryCode.json
+  const existingISOCC: string = "EP622373592US";
+  const nonExistentISOCC: string = "EP622373592ZZ";
+  const tooShort: string = "EP6223592U";
+  const tooLong: string = "EP622351412492US";
+  const specialCharacter: string = "EP622373592?!";
+
+  expect(validateISOCC(existingISOCC)).toBeTruthy();
+  expect(validateISOCC(nonExistentISOCC)).toBeFalsy();
+  expect(validateISOCC(tooShort)).toBeFalsy();
+  expect(validateISOCC(tooLong)).toBeFalsy();
+  expect(validateISOCC(specialCharacter)).toBeFalsy();
+});
+
+test("should validate string check digit", () => {
+  // 9th digit should be number that passes check after S10 calculation applied
+  const workingDigit: string = "EP622373592US";
+  const workingDigitNumber0: string = "EM112397940SE";
+  const workingDigitNumber5: string = "EU584344275US";
+  const notWorkingDigit: string = "EP622373590US";
+  const notWorkingDigit2: string = "EP111111112US";
+  const tooShort: string = "EP6223592U";
+  const tooLong: string = "EP622351412492US";
+  const specialCharacter: string = "EP622373592?!";
+  const specialCharacter2: string = "EP62237359£US";
+
+  expect(validateCheckDigit(workingDigit)).toBeTruthy();
+  expect(validateCheckDigit(workingDigitNumber0)).toBeTruthy();
+  expect(validateCheckDigit(workingDigitNumber5)).toBeTruthy();
+  expect(validateCheckDigit(notWorkingDigit)).toBeFalsy();
+  expect(validateCheckDigit(notWorkingDigit2)).toBeFalsy();
+  expect(validateCheckDigit(tooShort)).toBeFalsy();
+  expect(validateCheckDigit(tooLong)).toBeFalsy();
+  expect(validateCheckDigit(specialCharacter)).toBeTruthy(); // special character applied to letters not affected
+  expect(validateCheckDigit(specialCharacter2)).toBeFalsy();
+});
+
+test("should validate string for duplicates", () => {
+  // checks string for duplicates
+  const existingBarcode: string = "EP622373592US";
+  const nonExistingBarcode: string = "EM112397940SE";
+
+  expect(
+    checkForDuplicates(existingBarcode, barcodesJSON.barcodes)
+  ).toBeFalsy();
+  expect(
+    checkForDuplicates(nonExistingBarcode, barcodesJSON.barcodes)
+  ).toBeTruthy();
+});
